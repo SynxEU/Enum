2c3ddc9a939ee2b5d164b627bc81a869b36402ea
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-12-18 11:58:28 +0100

    Add project files.

---
+++ Enum/Farver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum Farver
+    {
+        Rød,
+        Grøn,
+        Blå,
+        Gul,
+        Sort
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main()
+        {
+            Farver valgtFarve = Farver.Blå;
+
+            UdskrivFarve(valgtFarve);
+        }
+
+        static void UdskrivFarve(Farver farve)
+        {
+            Console.WriteLine($"Valgt farve: {farve}");
+        }
+    }
+
+}
